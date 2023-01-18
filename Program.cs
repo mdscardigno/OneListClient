d59e59c427d1e2a802f14b726907ca830694732b
Milagro Scardigno <mdscardigno@gmail.com>
2023-01-18 17:16:25 -0500

    handling excemtions for lists and single items

--- Program.cs
+++ Program.cs
@@ -31,13 +31,21 @@ namespace OneListClient
             while (keepGoing)
             {
                 Console.Clear();
-                Console.WriteLine("Get (A)ll Todo, or (Q)uit");
+                Console.WriteLine("Get (A)ll Todo, (O)ne Todo or (Q)uit");
                 var choice = Console.ReadLine().ToUpper();
 
                 switch (choice)
                 {
                     case "A":
                         await ShowAllItemsAsync(token);
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+                        break;
+                    case "O":
+                        Console.WriteLine("Enter the ID of the item you want to see");
+                        var id = int.Parse(Console.ReadLine());
+
+                        await GetOneItemAsync(token, id);
                         Console.WriteLine("Press ENTER to continue");
                         Console.ReadLine();
                         break;
@@ -50,36 +58,75 @@ namespace OneListClient
                 }
             }
 
+            static async Task GetOneItemAsync(string token, int id)
+            {
+                try
+                {
+                    // throw new NotImplementedException();
+                    var client = new HttpClient();
+
+                    var responseBodyAsStream = await client.GetStreamAsync($"https://one-list-api.herokuapp.com/items/{id}?access_token={token}");
+
+                    //                                              Describe the Shape of the Data (Object in JSON => Item)
+                    //                                                  v   
+                    var item = await JsonSerializer.DeserializeAsync<Item>(responseBodyAsStream);
+
+                    var table = new ConsoleTable("Description".Pastel(Color.Red), "Created At".Pastel(Color.Red), "Completed Status".Pastel(Color.Red));
+                    //we do not loop because we have one item
+                    table.AddRow(item.Text, item.CreatedAt, item.CompletedStatus);
+
+                    table.Write();
+                }
+                catch (HttpRequestException)
+                {
+                    Console.WriteLine("I could not find that item");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    //different error message
+
+                }
+            }
+
             static async Task ShowAllItemsAsync(string token)
             {
-                // throw new NotImplementedException();
-                //var responseBodyAsString = await client.GetStringAsync("https://one-list-api.herokuapp.com/items?access_token=cohort22");//turning this into an sync
-
-                var client = new HttpClient();
-                //list i am using is illustriousvoyage
-                var responseBodyAsStream = await client.GetStreamAsync($"https://one-list-api.herokuapp.com/items?access_token={token}");
-
-                //turning this into an sync
-                //this code will happen at the same time our network request is happening
-                //we cannot Console.WriteLine a stream, so we need to deserialize it by feeding it to something called a JSON serializer
-                //Console.WriteLine(responseBodyAsString);
-                //                                      Describe the Shape of the Data (array in JSON => List, Object => Item class )
-                //                                                  v   v
-                var items = await JsonSerializer.DeserializeAsync<List<Item>>(responseBodyAsStream);//turning this into an sync
-                                                                                                    //Back in the world of List/LINQ/C#
-
-                //using Console Tables for fancy output
-                var table = new ConsoleTable("Description".Pastel(Color.Red), "Created At".Pastel(Color.Red), "Completed Status".Pastel(Color.Red));
-
-
-
-                foreach (var item in items)
+                try
                 {
-                    table.AddRow(item.Text, item.CreatedAt, item.CompletedStatus);
-                }
+                    // throw new NotImplementedException();
+                    //var responseBodyAsString = await client.GetStringAsync("https://one-list-api.herokuapp.com/items?access_token=cohort22");//turning this into an sync
 
-                table.Write();
+                    var client = new HttpClient();
+                    //list i am using is illustriousvoyage
+                    var responseBodyAsStream = await client.GetStreamAsync($"https://one-list-api.herokuapp.com/items?access_token={token}");
+
+                    //turning this into an sync
+                    //this code will happen at the same time our network request is happening
+                    //we cannot Console.WriteLine a stream, so we need to deserialize it by feeding it to something called a JSON serializer
+                    //Console.WriteLine(responseBodyAsString);
+                    //                                      Describe the Shape of the Data (array in JSON => List, Object => Item class )
+                    //                                                  v   v
+                    var items = await JsonSerializer.DeserializeAsync<List<Item>>(responseBodyAsStream);//turning this into an sync
+                                                                                                        //Back in the world of List/LINQ/C#
+
+                    //using Console Tables for fancy output
+                    var table = new ConsoleTable("Description".Pastel(Color.Red), "Created At".Pastel(Color.Red), "Completed Status".Pastel(Color.Red));
+
+
+
+                    foreach (var item in items)
+                    {
+                        table.AddRow(item.Text, item.CreatedAt, item.CompletedStatus);
+                    }
+
+                    table.Write();
+                }
+                catch (HttpRequestException)
+                {
+                    Console.WriteLine("I could not find that list");
+                }
             }
         }
+
+
     }
 }
