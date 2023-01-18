f1b25b6109f1e40fcd1be87ffb59e9dc30b60bc4
Milagro Scardigno <mdscardigno@gmail.com>
2023-01-18 13:37:17 -0500

    GET Request

--- Program.cs
+++ Program.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace OneListClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args) //this changed from void to async Task
         {
             Console.WriteLine("Welcome to OneListClient");
             var client = new HttpClient();
 
-
+            var responseBodyAsString = await client.GetStringAsync("https://one-list-api.herokuapp.com/items?access_token=cohort22");//turning this into an sync
+            //this code will happen at the same time our network request is happening
+            Console.WriteLine(responseBodyAsString);
         }
     }
 }
