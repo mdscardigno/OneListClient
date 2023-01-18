cd16d7a7b312d2a4393c6a00575da9ddec8a5982
Milagro Scardigno <mdscardigno@gmail.com>
2023-01-18 15:31:11 -0500

    changing the getter to return condition in ternary

--- Item.cs
+++ Item.cs
@@ -4,11 +4,35 @@ namespace OneListClient
 {
     public class Item
     {
-        public int id { get; set; }
+        public int id { get; set; }//default getter and setter
         public string text { get; set; }
         public bool complete { get; set; }
         public DateTime created_at { get; set; }
         public DateTime updated_at { get; set; }
         //note the property names are the same as the JSON keys and match the case
+
+        //what if costumed getter and setter
+        public string CompletedStatus
+        {//read only, code supported property
+            get
+            {
+                return complete ? "Complete" : "Incomplete";
+                // if (complete)
+                // {
+                //     return "Complete";
+                // }
+                // else
+                // {
+                //     return "Incomplete";
+                // }
+            }
+        }
+        //c# ternary operator: 
+        //boolean expression ? value when true : value when false
+        //condition ? statement 1 : statement 2
+        //int x = 10, y = 100;
+        //var result = x > y ? "x is greater than y" : "x is less than y";
+        //output is "x is less than y"
+
     }
 }
