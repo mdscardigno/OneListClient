cc1dcc1d83c1f0ff115da026a698fbc5dfb140aa
Milagro Scardigno <mdscardigno@gmail.com>
2023-01-18 17:57:21 -0500

    create an item in the todo list

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
 using ConsoleTables;
@@ -31,7 +32,7 @@ namespace OneListClient
             while (keepGoing)
             {
                 Console.Clear();
-                Console.WriteLine("Get (A)ll Todo, (O)ne Todo or (Q)uit");
+                Console.WriteLine("Get (A)ll Todo, (C)reate a New Item, (O)ne Todo or (Q)uit");
                 var choice = Console.ReadLine().ToUpper();
 
                 switch (choice)
@@ -49,6 +50,17 @@ namespace OneListClient
                         Console.WriteLine("Press ENTER to continue");
                         Console.ReadLine();
                         break;
+                    case "C":
+                        Console.WriteLine("Enter the description of your new todo: ");
+                        var text = Console.ReadLine();
+
+                        var newItem = new Item { Text = text };
+
+                        await AddOneItem(token, newItem);
+
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+                        break;
                     case "Q":
                         keepGoing = false;
                         break;
@@ -56,6 +68,38 @@ namespace OneListClient
                         Console.WriteLine("Invalid choice");
                         break;
                 }
+            }
+            static async Task AddOneItem(string token, Item newItem)
+            {
+                // throw new NotImplementedException();
+                var client = new HttpClient();
+                var url = $"https://one-list-api.herokuapp.com/items?access_token={token}";
+
+                //we are going the other way around. we are turning an object into a string
+                var jsonBody = JsonSerializer.Serialize(newItem);
+
+                //we turn the object into a StringContent object and indicate we are using JSON
+                //ensuring there is a media type header of application/json
+
+                var jsonBodyAsContent = new StringContent(jsonBody);
+                // jsonBodyAsContent.Headers.ContentType.MediaType = "application/json";
+                jsonBodyAsContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                //we ask our client to Send the POST async request to the URL and supply the JSON body
+                var response = await client.PostAsync(url, jsonBodyAsContent);
+
+                //Get the response as a stream
+                var responseJSON = await response.Content.ReadAsStreamAsync();
+
+                //Supply that stream of data to Deserialize that will interpret it as a single item
+                var item = await JsonSerializer.DeserializeAsync<Item>(responseJSON); //turning this into an sync
+
+                //make a table to output our new item
+                var table = new ConsoleTable("ID".Pastel(Color.Red), "Description".Pastel(Color.Red), "Created At".Pastel(Color.Red), "Updated At".Pastel(Color.Red), "Completed".Pastel(Color.Red));
+                table.AddRow(item.Id, item.Text, item.CreatedAt, item.UpdatedAt, item.CompletedStatus);
+
+                //write the table to the console
+                table.Write(Format.Minimal);
             }
 
             static async Task GetOneItemAsync(string token, int id)
