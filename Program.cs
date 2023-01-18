bbb09eeddbaf5439b1a564164e3a2124e6eb5402
Milagro Scardigno <mdscardigno@gmail.com>
2023-01-18 11:07:25 -0500

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace OneListClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to OneListClient");
+            var client = new HttpClient();
+
+
+        }
     }
-  }
 }
